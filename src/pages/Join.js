73916dc0aba1df96151dfc316452c5710be3adcf
Undong00 <ehddjs85246@gmail.com>
2023-05-06 03:33:57 +0900

    댓글삭제  , 라우터, 수정기능 버튼 추가

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
+// 회원가입
 
 function Join() {
-    return (
-        <div>
-            <h1>JOIN입니다.</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1>JOIN입니다.</h1>
+    </div>
+  );
 }
 
 export default Join;
--- src/pages/Quiz.js
+++ src/pages/Quiz.js
@@ -2,11 +2,24 @@ import React from "react";
 import Body from "../components/Body";
 import Title from "../components/Title";
 import Comment from "../components/Comment";
+import { useDispatch, useSelector } from "react-redux";
+import { isEdit } from "../redux/modules/componentMode";
+// 수정 시 버튼 바꾸기,
 function Quiz() {
+  // 액션객체를 시행하기 위핸 디스패쳐를 선언한다.
+  const dispatch = useDispatch();
+  //전역 스토어에서 현재 게시글이 수정상태인지 여부를 가져온다.
+  const isEditMode = useSelector((state) => state.componentMode.isEdit);
+  // 디스패쳐를 통해 import한 isEdit에 값을 넘겨준다.
+  // true, false를 명시적으로 넘겨줌. 수정모드 하고시으면 이걸 트루로 버튼에서 바꿔준다.
+  const handleButtonClick = () => {
+    dispatch(isEdit(!isEditMode));
+  };
   return (
     <div>
       <Title />
-      <button>hi</button>
+      <button>삭제하기</button>
+      <button onClick={handleButtonClick}>{isEditMode ? "✍️" : "✅"}</button>
       <Body />
       <Comment />
     </div>
