ed04f4db032452d501da3f5fbe79c693ef35563b
Undong00 <ehddjs85246@gmail.com>
2023-05-06 14:20:50 +0900

    로그인, 회원가입 추가

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,28 +1,28 @@
-import React from "react";
-// 등록하기
+// import React from "react";
+// // 등록하기
 
-const [users, setUsers] = useState([
-  {
-    id: 1,
-    title: title,
-    body: body,
-  },
-]);
-// 퀴즈 등록
-const addQuiz = () => {
-  const newQuiz = {
-    id: users.length + 1,
-    quiz: "",
-  };
-  setUsers([...users, newQuiz]);
-};
+// const [users, setUsers] = useState([
+//   {
+//     id: 1,
+//     title: title,
+//     body: body,
+//   },
+// ]);
+// // 퀴즈 등록
+// const addQuiz = () => {
+//   const newQuiz = {
+//     id: users.length + 1,
+//     quiz: "",
+//   };
+//   setUsers([...users, newQuiz]);
+// };
 
-function Register() {
-  return (
-    <div>
-      <h1>레지스터입니다.</h1>
-    </div>
-  );
-}
+// function Register() {
+//   return (
+//     <div>
+//       <h1>레지스터입니다.</h1>
+//     </div>
+//   );
+// }
 
-export default Register;
+// export default Register;
--- src/pages/Test.js
+++ src/pages/Test.js
@@ -1,33 +1,29 @@
+// import React from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { isEdit } from '../redux/modules/componentMode';
+// import Answer from '../components/Answer';
 
-import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { isEdit } from '../redux/modules/componentMode';
-import Answer from '../components/Answer';
+// // function Test() {
+// //   //전역 스토어에서 현재 게시글이 수정상태인지 여부를 가져온다.
+// //   const isEditMode = useSelector((state) => {
+// //     return state.componentMode.IsEdit;
+// //   });
 
+// //   // 액션객체를 시행하기 위핸 디스패쳐를 선언한다.
+// //   const dispatcher = useDispatch();
 
-// function Test() {
-//   //전역 스토어에서 현재 게시글이 수정상태인지 여부를 가져온다.
-//   const isEditMode = useSelector((state) => {
-//     return state.componentMode.IsEdit;
-//   });
+// //   // 디스패쳐를 통해 import한 isEdit에 값을 넘겨준다.
+// //   // true, false를 명시적으로 넘겨줌. 수정모드 하고시으면 이걸 트루로 버튼에서 바꿔준다.
+// //   dispatcher(isEdit(false));
 
-//   // 액션객체를 시행하기 위핸 디스패쳐를 선언한다.
-//   const dispatcher = useDispatch();
+//     return (
+//         <div>
+//             TEST
+//             <Answer>
 
-//   // 디스패쳐를 통해 import한 isEdit에 값을 넘겨준다.
-//   // true, false를 명시적으로 넘겨줌. 수정모드 하고시으면 이걸 트루로 버튼에서 바꿔준다.
-//   dispatcher(isEdit(false));
+//             </Answer>
+//         </div>
+//     );
+// }
 
-
-    return (
-        <div>
-            TEST
-            <Answer>
-
-                
-            </Answer>
-        </div>
-    );
-}
-
-// export default Test;
+// // export default Test;
